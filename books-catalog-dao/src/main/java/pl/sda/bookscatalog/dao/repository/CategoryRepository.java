24c1c512e96ae5afe642b159d56b31664d4dbf1c
Lukasz Oremczuk <l.oremczuk@gmail.com>
2017-06-07 19:53:49 +0200

    added CategoryRepository Interface

---
+++ books-catalog-dao/src/main/java/pl/sda/bookscatalog/dao/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package pl.sda.bookscatalog.dao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.bookscatalog.dao.domain.Category;
+
+/**
+ * Created by RENT on 2017-06-07.
+ */
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
