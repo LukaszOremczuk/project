ae05b639c0185956986fa76ceda6afaed8073bdc
Jacek Sabat <jacek.sabat66@gmail.com>
2017-06-07 20:37:28 +0200

    dodanie: pakietów do service

---
+++ books-catalog-service/src/main/java/pl/sda/bookscatalog/service/book/exception/BookAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package pl.sda.bookscatalog.service.book.exception;
+
+/**
+ * Created by jacek on 07.06.17.
+ */
+public class BookAlreadyExistsException {
+}
---
+++ books-catalog-service/src/main/java/pl/sda/bookscatalog/service/book/query/BookQueryService.java
@@ -0,0 +1,7 @@
+package pl.sda.bookscatalog.service.book.query;
+
+/**
+ * Created by jacek on 07.06.17.
+ */
+public class BookQueryService {
+}
