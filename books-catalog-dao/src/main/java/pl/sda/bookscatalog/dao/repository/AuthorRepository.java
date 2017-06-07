873e5b04fc558aa85e463775766c2d3b106aa699
Jacek Sabat <jacek.sabat66@gmail.com>
2017-06-07 19:47:05 +0200

    dodanie biblioteki: spring-data, dodanie: AuthorRepository

--- books-catalog-dao/src/main/java/pl/sda/bookscatalog/dao/repository/AuthorRepository.java
+++ books-catalog-dao/src/main/java/pl/sda/bookscatalog/dao/repository/AuthorRepository.java
@@ -1,11 +1,14 @@
-//package pl.sda.bookscatalog.dao.repository;
-//
-//import pl.sda.bookscatalog.dao.domain.Author;
-//
-///**
-// * Created by jacek on 07.06.17.
-// */
-//
-//@Repository
-//public interface AuthorRepository extends JpaRepository<Author, Long>{
-//}
+package pl.sda.bookscatalog.dao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.bookscatalog.dao.domain.Author;
+
+/**
+ * Created by jacek on 07.06.17.
+ */
+
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
