00c4ae4333a9158982f7cf37ab0281d3123f20ed
Jacek Sabat <jacek.sabat66@gmail.com>
2017-06-09 18:31:30 +0200

    dodanie: WebConfig, WebAppInitializer, pom.web

---
+++ books-catalog-web/src/main/java/pl/sda/bookscatalog/web/configuration/WebAppInitializer.java
@@ -0,0 +1,60 @@
+package pl.sda.bookscatalog.web.configuration;
+
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by jacek on 09.06.17.
+ */
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext applicationContext = getApplicationContext();
+
+        servletContext.addListener(new ContextLoaderListener(applicationContext));
+
+        registerCustomFilters(servletContext);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher",
+                new DispatcherServlet(getDispatcherContext()));
+        configureDispatcher(dispatcher);
+    }
+
+    private void registerCustomFilters(ServletContext servletContext) {
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encodingFilter",
+                new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
+
+    private void configureDispatcher(ServletRegistration.Dynamic dispatcher) {
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+    private AnnotationConfigWebApplicationContext getApplicationContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(SecurityConfig.class);
+        context.register(WebConfig.class);
+
+        return context;
+    }
+
+    private AnnotationConfigWebApplicationContext getDispatcherContext() {
+        AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(WebConfig.class);
+
+        return dispatcherContext;
+    }
+}
---
+++ books-catalog-web/src/main/java/pl/sda/bookscatalog/web/configuration/WebConfig.java
@@ -0,0 +1,62 @@
+package pl.sda.bookscatalog.web.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
+
+/**
+ * Created by jacek on 09.06.17.
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.sda.bookscatalog.web.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+//    @Bean
+//    public MessageSource messageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasename("i18n.messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        messageSource.setUseCodeAsDefaultMessage(true);
+//
+//        return messageSource;
+//    }
+
+//    @Bean
+//    public LocaleChangeInterceptor localeChangeInterceptor() {
+//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+//        localeChangeInterceptor.setParamName("language");
+//
+//        return localeChangeInterceptor;
+//    }
+
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(localeChangeInterceptor());
+//    }
+
+
+
+}
